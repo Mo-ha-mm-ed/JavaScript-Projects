8e7c996c442aa5762db09f12fdf17b031134eaa2
Mohammed <i-mohammed@outlook.com>
2023-06-09 20:15:45 +0300

    Commiting Rock Papaer Scissor with loop where player can create a mach with number of rounds.

--- RockPaperGameWithLoop.js
+++ RockPaperGameWithLoop.js
@@ -1,79 +1,145 @@
 /*
 Oct 24, 2022
 This is the lagendary game [ Rock - Paper - Scissor]
+
+Fixed June 6, 2023
+
+The game start from line 95 and then the final score comparson starts from line 117.
+
 */
 
+/////////////// Changing number of Match is optional /////////////////////
+//Number of round to play. 
+let numberOfRoundToPlay = 10; ///Changeable
 
-//These are the selection which each user should select from.
+
+//List possible selection by the team User and Computer 
 const selectionArray = ['Rock', 'Paper', 'Scissor'];
 
-//These are some Array created to be filled with "1" point or "0" point.
-let userWinnings = ['s']; 
-let computerWinnings =  ['s'];
-
-//This function is to generate random number for each user
+//Generate random number for each user
 generatingRandomNumber = () => {
-  random = Math.floor(Math.random() * 3);
-  return random;
+    random = Math.floor(Math.random() * 3);
+    return random;
 };
 
-//////////////////
 
+//List to record the score points for each player
+var userWinnings = []; 
+var computerWinnings = [];
+
+
+
+//Winning Rules function. 
 function winner(userSelection, computerSelection){
+
     // if both user and compuer have the same selection
     if (userSelection === computerSelection){
-        winning = "No winner";
-    }
-    // Paper Beat Rock
-    if (userSelection === "Rock" && computerSelection === "Paper") {
-        computerWinnings.push("1");
-    }
-    if (userSelection === "Paper" && computerSelection === "Rock") {
-        userWinnings.push("1");
-    }
-
-    //Rock Beats Scissor
-    if (userSelection === "Rock" && computerSelection === "Scissor") {
-        userWinnings.push("1");
-    }
-    if (userSelection === "Scissor" && computerSelection === "Rock") {
-        computerWinnings.push("1");
-    }
-
-    //Scissor Beat Paper
-    if (userSelection === "Scissor" && computerSelection === "Paper") {
-        userWinnings.push("1");
-    }
-    if (userSelection === "Paper" && computerSelection === "Scissor") {
-        computerWinnings.push("1");
-    }
-}    
-
-
-//////////////////
-
-
-
-playing = true;
-
-while ((userWinnings.length) != 11 || (computerWinnings.length) != 11) {
-    userSelection = selectionArray [generatingRandomNumber()];
-    computerSelection = selectionArray[[generatingRandomNumber()]];
-    //console.log(`User has selected ====> ${userSelection}     Computer has selected ${computerSelection} `);
-    winner(userSelection,computerSelection);
-    if (userWinnings.length === 11 || computerWinnings.length ===11) {
-        if (userWinnings.length ===11) {
-            winningAnnouncement = "User won 10 games.....";
-            console.log(winningAnnouncement);
-            playing = false;
-        } else if (computerWinnings.length === 11) {
-            winningAnnouncement = "Computer won 10 games.....";
-            console.log(winningAnnouncement);
-            playing = false;
-        }
-    } else {
-      playing = true;
+        theWinner = "No winner";
+        return theWinner;
+    } else if (userSelection === "Rock" && computerSelection === "Paper") {
+        // Paper beats Rock Computer wins
+        computerWinnings.push("1"); //Add 1 to winning array
+        theWinner = "Computer";
+        return theWinner;
+    } else if (userSelection === "Paper" && computerSelection === "Rock") {
+        //Paper beats Rock User wins
+        userWinnings.push("1"); //Add 1 to winning array
+        theWinner = "User";
+        return theWinner;
+    } else if (userSelection === "Rock" && computerSelection === "Scissor") {
+        //Rock beats Scissor User wins
+        userWinnings.push("1"); //Add 1 to winning array
+        theWinner = "User";
+        return theWinner;
+    } else if (userSelection === "Scissor" && computerSelection === "Rock") {
+        //Rock beats Scissor Computer wins
+        computerWinnings.push("1"); //Add 1 to winning array
+        theWinner = "Computer";
+        return theWinner;
+    } else if (userSelection === "Scissor" && computerSelection === "Paper") {
+        //Paper beats Scissor User wins
+        userWinnings.push("1"); //Add 1 to winning array
+        theWinner = "User";
+        return theWinner;
+    } else if (userSelection === "Paper" && computerSelection === "Scissor") {
+         //Paper beats Scissor Computer wins
+        computerWinnings.push("1"); //Add 1 to winning array
+        theWinner = "Computer";
+        return theWinner;
     };
-  } ;
+};
+
+/////////// Game Begins ///////// //////// ////////////////
+console.log(" ");
+console.log("Hello..."); //Greeting message
+console.log(" ");
+console.log("Welcome to the fun game Rock Paper Scissor Game......");
 
 
+//Rounds output statement. This will output Rounds with s if it is plural, or without s if it is singular.
+if (numberOfRoundToPlay < 2) {
+    console.log(" ");
+    console.log(`A Game of ${numberOfRoundToPlay} Round Only Has Started...`);
+    console.log(" ");
+    console.log(" ");
+} else {
+    console.log(" ");
+    console.log(`A Game of ${numberOfRoundToPlay} Rounds Has Started...`);
+    console.log(" ");
+    console.log(" ");
+};
+
+//This is a for loop for the game, For every round.
+//From round 1 going upward...
+for (var round = 1; round <= numberOfRoundToPlay; round++) {
+    //Setting selection for every round.
+    userSelection = selectionArray[generatingRandomNumber()];
+    computerSelection = selectionArray[generatingRandomNumber()];
+
+    //Output every round with the round details and user score change. 
+    console.log("////////////");
+    console.log(`    ===>         Round ${round} started`);
+    console.log(`           User selected ${userSelection} ...`);
+    console.log(" ");
+    console.log(`           Computer selected ${computerSelection} ...`);
+    console.log(" ");
+    console.log(`           The winner is in round ${round} is ${winner(userSelection, computerSelection)}`);
+    console.log(" ");
+    console.log(`                   User current score is ${userWinnings.reduce((accumulator, currentValue) => accumulator + (+currentValue), 0)}`)
+    console.log(" ");
+    console.log(`                   Computer current score is ${computerWinnings.reduce((accumulator, currentValue) => accumulator + (+currentValue), 0)}`)
+    console.log(`    ===>         Round ${round} ended`);
+    console.log("/////$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$////");
+    console.log(" ");
+    console.log(" ");
+};
+
+//Final result output analysis. This will compare user and computer score and see who won.
+let userScore = userWinnings.reduce((accumulator, currentValue) => accumulator + (+currentValue), 0);
+let computerScore = computerWinnings.reduce((accumulator, currentValue) => accumulator + (+currentValue), 0)
+console.log(`$$$$$$$$$$$$$$$$   The Match of ${numberOfRoundToPlay} round/s is over   $$$$$$$$$$$$$$$$$$`);
+console.log(" ");
+if (userScore < computerScore) {
+    // if user score is less than computer score (Computer Won)
+    differenceInScore = Math.abs(computerScore - userScore);
+    console.log(`The winner is Computer with the total score of ${computerScore} points.`);
+    console.log(" ");
+    console.log(`The User scored only  ${userScore} points.`);
+    console.log(" ");
+    console.log(`This is make the computer ${differenceInScore} points higher than user`);
+    console.log(" ");
+} else if (userScore > computerScore){
+    // if computer score is less than computer score. (user Won)
+    differenceInScore = Math.abs(computerScore - userScore);
+    console.log(`The winner is User with the total score of ${userScore} points.`);
+    console.log(" ");
+    console.log(`The Computer scored only  ${computerScore} points.`);
+    console.log(" ");
+    console.log(`This is make the User ${differenceInScore} points higher than user`);
+    console.log(" ");
+} else {
+    console.log(`Hard game. Unfortunatly no winner.`);
+    console.log(" ");
+    console.log(`The computer got  ${computerScore} points and the user fot ${userScore} points`);
+    console.log(" ");
+};
