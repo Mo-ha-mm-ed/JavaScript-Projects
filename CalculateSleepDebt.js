41fb18b815402f7fc09917c0cd5cb14484c4f5d4
Mohammed <i-mohammed@outlook.com>
2023-06-11 01:06:31 +0300

    Update sleep calculating hours. Adding a check for everyday slept less, more, or equal than 8 hours.

--- CalculateSleepDebt.js
+++ CalculateSleepDebt.js
@@ -1,87 +1,193 @@
 /*
 Oct 20, 2022
-Welcome. The purpose of this program is to calculate the user's sleep debt.
-We will be doing that by determining how many hours the user slept in a whole week, 
-then we will take this actual data then compare it with ideal sleep data. 
 
-Finaly, we will output the result to the user by showing the user the actual number of 
-hours slept for the whole week, the ideal hours, then comparing them.
+This program calculate your sleep debt. by getting the number of hours 
+you slept in a whole week, then it will output 
+- the actual sleep hours
+- the ideal sleep hours
+- Compare the hours
+
+Fixed Jun 11, 2023
 
 */
 
-// Let us welcome the user:
+/////               =========>                        Input Section (Before program runs).  <<<<<<<<<<<<<<<
 
-console.log("  ");
-console.log(" ------      Hello and Welcome   to  ------           ");
-console.log(" -----     The Sleep Debt Calculator   -----       ");
-console.log("  ");
+// Get Daily sleep hours from user.
 
-// Then we will get the number of hours the user slept for the week. 
+const Sleep_Sunday = 1; //User provided
+const Sleep_Monday = 4; //User provided
+const Sleep_Tuesday = 8; //User provided
+const Sleep_Wednesday = 7; //User provided
+const Sleep_Thursday = 6; //User provided
+const Sleep_Friday = 2; //User provided
+const Sleep_Saturday = 4; //User provided
 
-const getSleepHours = (day) => {
-  if (day === "sunday") {
-    return 8;
-  } else if (day === "monday") {
-    return 7;
-  } else if (day === "tuesday") {
-    return 6;
-  } else if (day === "wednesday") {
-    return 5;
-  } else if (day === "thursday") {
-    return 4;
-  } else if (day === "friday") {
-    return 3;
-  } else if (day === "saturday") {
-    return 2;
-  } else {
-    return "Error. Please try again"
-  }
-}
+//Total sleep of the whole week
+const userSleepActualHours = (Sleep_Sunday + Sleep_Monday + Sleep_Tuesday + Sleep_Wednesday + Sleep_Thursday + Sleep_Friday + Sleep_Saturday); // Total hours of sleep the whole week by user
 
-console.log(" ")
+/////    ===============> Input done  <<<<<<<<<<<<<<<
 
-//Now let us get the actual sleep hours for the whole week. 
-
-const getActualSleepHours = () => {
-  return ( getSleepHours("sunday") + getSleepHours("monday") + getSleepHours("tuesday") + getSleepHours("wednesday") +  getSleepHours("thursday") + getSleepHours("friday") + getSleepHours("saturday")  );
-}
-
-console.log(" ")
+// The ideal sleep hours
+const idealSleepHoursDaily = 8;
+const idealSleepingHoursWeek = (idealSleepHoursDaily * 7);
 
 
-//Get Ideal Hours calculation
-const getIdealSleepHours = () => {
-  const idealHours = 8; //This is the ideal hours per day
-  return idealHours * 7; //This is ideal hours er week
-}
+//Check day of the week and compare it.
+// Check if the weekly hours of sleep matches the ideal
+if  (userSleepActualHours === idealSleepingHoursWeek ) {
+  console.log("It seems you have slept the ideal weekly hours. Great job");
+  console.log(" ");
+
+} else {
+  //If the actual hours Does not match the ideal
+  // find the days where user slept less or more:
+
+  console.log("It seems that you have slept less than the ideal weekly hours. I can see from the data you provided that you slept less than 8 hours on the days below. It seems that you have a disturbed schedule");
+  console.log(" ");
+
+  if (Sleep_Sunday < idealSleepHoursDaily) {
+    console.log("Sunday " + Sleep_Sunday + " hours."); 
+    console.log(" ");
+  };
+
+  if (Sleep_Monday < idealSleepHoursDaily) {
+    console.log("Monday " + Sleep_Monday + " hours."); 
+    console.log(" "); 
+  };
+
+  if (Sleep_Tuesday < idealSleepHoursDaily) {
+    
+    console.log("Tuesday " + Sleep_Tuesday + " hours.");  
+    console.log(" ");
+  };
+
+  if (Sleep_Wednesday < idealSleepHoursDaily) {
+    
+    console.log("Wednesday " + Sleep_Wednesday + " hours."); 
+    console.log(" "); 
+  };
+
+  if (Sleep_Thursday < idealSleepHoursDaily) {
+    
+    console.log("Thursday " + Sleep_Thursday + " hours."); 
+    console.log(" ");
+  };
+
+  if (Sleep_Friday < idealSleepHoursDaily) {
+   
+    console.log("Friday " + Sleep_Friday + " hours."); 
+    console.log(" ");
+  };
+
+  if (Sleep_Saturday < idealSleepHoursDaily) {
+    console.log("Sunday " + Sleep_Saturday + " hours."); 
+    console.log(" ");
+  };
+
+  console.log(" ");
+  console.log(" ");
+  console.log(" ");
+
+  //Check if sleep hours is greater.
+  if (Sleep_Sunday > idealSleepHoursDaily) {
+    console.log("Sunday " + Sleep_Saturday + " hours.");
+    console.log("This day you slept longer than 8");  
+    console.log(" ");
+  };
+
+  if (Sleep_Monday > idealSleepHoursDaily) {
+ 
+    console.log("Monday " + Sleep_Monday + " hours.");  
+    console.log("This day you slept longer than 8");
+    console.log(" ");
+  };
+
+  if (Sleep_Tuesday > idealSleepHoursDaily) {
+
+    console.log("Tuesday " + Sleep_Tuesday + " hours."); 
+    console.log("This day you slept longer than 8");
+    console.log(" ");
+  };
+
+  if (Sleep_Wednesday > idealSleepHoursDaily) {
+
+    console.log("Wednesday " + Sleep_Wednesday + " hours."); 
+    console.log("This day you slept longer than 8");
+    console.log(" ");
+  };
+
+  if (Sleep_Thursday > idealSleepHoursDaily) {
+  
+    console.log("Thursday " + Sleep_Thursday + " hours."); 
+    console.log("This day you slept longer than 8");
+    console.log(" ");
+  };
+
+  if (Sleep_Friday > idealSleepHoursDaily) {
+ 
+    console.log("Friday " + Sleep_Friday + " hours."); 
+    console.log("This day you slept longer than 8");
+    console.log(" ");
+  };
+
+  if (Sleep_Saturday > idealSleepHoursDaily) {
+
+    console.log("Saturday " + Sleep_Saturday + " hours."); 
+    console.log("This day you slept longer than 8");
+    console.log(" ");
+  };
 
 
-///Third Calculate final step Of Calculating sleep debt
+  //Check if user slept exactly 8 hours
+  if (Sleep_Sunday === idealSleepHoursDaily) {
+
+    console.log("Sunday " + Sleep_Sunday + " hours."); 
+    console.log("This day is normal 8 hours");
+    console.log(" ");
+  };
+
+  if (Sleep_Monday === idealSleepHoursDaily) {
+
+    console.log("Monday " + Sleep_Monday + " hours."); 
+    console.log("This day is normal 8 hours");
+    console.log(" ");
+  };
+
+  if (Sleep_Tuesday === idealSleepHoursDaily) {
+
+    console.log("Tuesday " + Sleep_Tuesday + " hours."); 
+    console.log("This day is normal 8 hours");
+    console.log(" ");
+  };
+
+  if (Sleep_Wednesday === idealSleepHoursDaily) {
+
+    console.log("Wednesday " + Sleep_Wednesday + " hours."); 
+    console.log("This day is normal 8 hours");
+    console.log(" ");
+  };
+
+  if (Sleep_Thursday === idealSleepHoursDaily) {
+
+    console.log("Thursday " + Sleep_Thursday + " hours."); 
+    console.log("This day is normal 8 hours");
+    console.log(" ");
+  };
+
+  if (Sleep_Friday === idealSleepHoursDaily) {
+
+    console.log("Friday " + Sleep_Friday + " hours."); 
+    console.log("This day is normal 8 hours");
+    console.log(" ");
+  };
+
+  if (Sleep_Saturday === idealSleepHoursDaily) {
+
+    console.log("Saturday " + Sleep_Saturday + " hours."); 
+    console.log("This day is normal 8 hours");
+    console.log(" ");
+  };
 
 
-const calculateSleepDebt = () => {
-    if (getActualSleepHours() === getIdealSleepHours()) {
-        console.log(`You have slept this week for ${getActualSleepHours()} hours. You got the perfect  amount of sleep...`);
-    } else if (getActualSleepHours() < getIdealSleepHours()) {
-        console.log(`You have slept this week for ${getActualSleepHours()} hours. You got less amount of sleep than you needed. You need to have ${getIdealSleepHours()} hours for the perfect sleep. Sadly, you are short by ${(getIdealSleepHours() ) - ( getActualSleepHours())} Hours.`);
-    } else if (getActualSleepHours() > getIdealSleepHours()) {
-        console.log(`You have slept this week for ${getActualSleepHours()} hours. You got more amount of sleep than you needed. `)
-    } else {
-        console.log("Error. Please check your what you entered and try again...")
-    }
 };
-
-
-
-
-// Print Output
-
-calculateSleepDebt();
-console.log(" ");
-console.log(" ");
-console.log(" ");
-console.log(" ");
-console.log(" -------       The End........   -------                ")
-console.log("  ");
-console.log("  ");
-console.log("  ");
